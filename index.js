f43cfdf600c2f80fbf9df5f224207777db86cda1
Artem Gladkov <mamifero3004@gmail.com>
2023-07-31 23:27:24 +0200

    sound fix

--- index.js
+++ index.js
@@ -2,28 +2,23 @@ const express = require("express");
 const ytdl = require("ytdl-core");
 const app = express();
 const PORT = process.env.PORT || 3000;
-
 app.use(express.static(__dirname + "/public"));
-
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/public/index.html");
 });
-
 app.get("/download", async (req, res) => {
     const videoUrl = req.query.url;
     if (!videoUrl) {
         return res.status(400).send("Missing video URL");
     }
-
     try {
         const info = await ytdl.getInfo(videoUrl);
         const videoTitle = info.videoDetails.title.replace(/[^a-zA-Z0-9]/g, "_");
-        const format = ytdl.chooseFormat(info.formats, { quality: 'highest' });
         const filteredFormats = info.formats.filter((format) => {
-            return format.height && format.height >= 720;
+            return format.hasVideo && format.hasAudio && format.audioBitrate;
         });
-        const selectedFormat = ytdl.chooseFormat(filteredFormats, { quality: 'highest' });
-
+        filteredFormats.sort((a, b) => b.audioBitrate - a.audioBitrate);
+        const selectedFormat = filteredFormats[0];
         res.header("Content-Disposition", `attachment; filename="${videoTitle}.mp4"`);
         ytdl(videoUrl, {
             format: selectedFormat
@@ -33,7 +28,6 @@ app.get("/download", async (req, res) => {
         res.status(500).send("Error occurred");
     }
 });
-
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
 });
