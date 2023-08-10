8198fc9111aa3fc53c415f169e23c5f58b395b17
Artem Gladkov <mamifero3004@gmail.com>
2023-08-10 16:43:17 +0200

    ad-test-fix

--- index.js
+++ index.js
@@ -338,22 +338,23 @@ app.get("/info", async (req, res) => {
                         </form>
                     </div>
                 </div>
+
+
+                <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1431012531703295"
+                crossorigin="anonymous"></script>
+           <!-- Uno -->
+           <ins class="adsbygoogle"
+                style="display:block"
+                data-ad-client="ca-pub-1431012531703295"
+                data-ad-slot="5406578885"
+                data-ad-format="auto"
+                data-full-width-responsive="true"></ins>
+           <script>
+                (adsbygoogle = window.adsbygoogle || []).push({});
+           </script>
+
+
             </div>
-
-
-            <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1431012531703295"
-     crossorigin="anonymous"></script>
-<!-- Uno -->
-<ins class="adsbygoogle"
-     style="display:block"
-     data-ad-client="ca-pub-1431012531703295"
-     data-ad-slot="5406578885"
-     data-ad-format="auto"
-     data-full-width-responsive="true"></ins>
-<script>
-     (adsbygoogle = window.adsbygoogle || []).push({});
-</script>
-
 
             <script>
             function toggleMenu() {
